7a04d2bd95c88fc138690ef7057c17b654ed3079
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-08-10 22:53:30 -0300

    main: feat - Implemented task creation form and associated model with controller

--- src/main/java/br/com/alan/layout/controller/TaskController.java
+++ src/main/java/br/com/alan/layout/controller/TaskController.java
@@ -1,13 +1,20 @@
 package br.com.alan.layout.controller;
 
+import br.com.alan.layout.model.Task;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TaskController {
 
     @GetMapping("/create")
-    public String create() {
+    public String home() {
         return "create";
+    }
+
+    @PostMapping("/create")
+    public void create(Task task) {
+        System.out.println("O nome da tarefa é: " + task.getName());
     }
 }
---
+++ src/main/java/br/com/alan/layout/model/Task.java
@@ -0,0 +1,43 @@
+package br.com.alan.layout.model;
+
+import java.util.Date;
+
+public class Task {
+
+    public Task(Long id, String name, Date date) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+    }
+
+    public Task() {
+    }
+
+    private Long id;
+    private String name;
+    private Date date;
+
+    public void SetId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+}
