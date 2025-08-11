dba4ae73213c7ea65bcd98aaef8ecada773b0b40
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-08-10 23:17:32 -0300

    main: feat - Refactored "TaskController" to store tasks and added "list.html" template for task listing

--- src/main/java/br/com/alan/layout/controller/TaskController.java
+++ src/main/java/br/com/alan/layout/controller/TaskController.java
@@ -5,8 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class TaskController {
+
+    List<Task> tasks = new ArrayList<>();
 
     @GetMapping("/create")
     public String home() {
@@ -14,7 +19,10 @@ public class TaskController {
     }
 
     @PostMapping("/create")
-    public void create(Task task) {
-        System.out.println("O nome da tarefa é: " + task.getName());
+    public String create(Task task) {
+        Long id = tasks.size() + 1L;
+        tasks.add(new Task(id, task.getName(), task.getDate()));
+
+        return "list";
     }
 }
